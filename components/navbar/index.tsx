6bd722ec156872cd1fadfe8aba6e3d89d78e1075
abdelhadidevv <88690811+abdelhadidevv@users.noreply.github.com>
2023-05-25 10:33:08 +0300

    Add navbar

---
+++ components/navbar/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import { FC } from "react";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = () => {
+  return (
+    <nav className="backdrop-blur fixed w-full">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center">
+          <img src="/next.svg" className="h-6 sm:h-8 mr-3" alt="Next.js" />
+        </a>
+        <button
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0">
+            <li>
+              <Link
+                href="/"
+                className="block py-2 pl-3 pr-4 rounded text-white hover:text-blue-500"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/random-dog"
+                className="block py-2 pl-3 pr-4 text-white hover:text-blue-500"
+              >
+                Random Dog
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
